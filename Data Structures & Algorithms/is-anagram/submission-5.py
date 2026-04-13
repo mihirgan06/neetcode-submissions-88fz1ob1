2494569369a306176430184db5a5734600c7eb85
Mihir Ganesan <mihirgan06@gmail.com>
2026-04-13 11:42:23 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        '''
+            Given two strings s and t, return true if the two strings are anagrams
+            Else return false
+        '''
+        from collections import defaultdict
+        if len(s) != len(t):
+            return False
+        counts_s = defaultdict(int)
+        counts_t = defaultdict(int)
+
+        for ch in s:
+            counts_s[ch] += 1
+        for ch in t:
+            counts_t[ch] += 1
+        for key in counts_s.keys():
+            if counts_s[ch] != counts_t[ch]:
+                return False
+        return True
+
+            
+        
+
+        
