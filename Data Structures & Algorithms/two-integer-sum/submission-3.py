24c1af0a2c27c638a389bbcb0eff238bd40f54de
Mihir Ganesan <mihirgan06@gmail.com>
2026-04-13 12:06:18 -0400

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        '''
+            Return indices i and j such that nums[i] + nums[j] == target
+            i cannot = j
+        
+        '''
+        from collections import defaultdict
+
+        my_map = defaultdict(int)
+        res = []
+        for i, num in enumerate(nums):
+            complement = target - nums[i]
+            if complement in my_map:
+                return [my_map[complement], i]
+            my_map[num] = i
+        return res
+
+        
