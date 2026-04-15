d42f969aab75290304b4f0efddb40ed243d6fd45
Mihir Ganesan <mihirgan06@gmail.com>
2026-04-15 11:54:36 -0400

    Update: anagram-groups - submission-2

--- Data Structures & Algorithms/anagram-groups/submission-2.py
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -1,18 +1,29 @@
-from collections import Counter
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list) #mapping charCount to list of anagrams
+        '''
+        Given array of strs
+        group anagrams into sublists and return
+        '''
+        from collections import defaultdict
+
+        groups = defaultdict(list)
+        res = []
+
         for word in strs:
-            count = [0] * 26 #26 0s for each character (a...z)
-
+            count = [0] * 26
+            #crerate count array for 26 letters
             for c in word:
-                count[ord(c) - ord("a")] += 1
+                #find the index in counts by subtracting ord('a')
+                index = ord(c) - ord('a')
+                count[index] += 1
+            key = tuple(count) 
+            #we have to pass in a tuple bc its immutable
+            groups[key].append(word)
+        return list(groups.values())
             
-            res[tuple(count)].append(word)
+            
+            
+            
+            
         
-        return list(res.values())
-
-            
-
         
