70e4665105c5878067d3f1006c5c9919539b1c32
Mihir Ganesan <mihirgan06@gmail.com>
2026-04-23 16:38:32 -0400

    Add: evaluate-reverse-polish-notation - submission-13

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-13.py
@@ -0,0 +1,46 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        '''
+            Given array of strings tokens
+            we can get smt like 
+            1, 2, +, 3, *
+            and need to do 
+            (1+2)*3
+
+            makes sense makes sense
+            so we just need an O(N) solution to this
+            create a stack
+            iterate through the tokens
+            first push the first number
+
+            we want to pop the correct value, so do we just cimpute it like and return stacj, idts
+
+            
+
+        '''
+        stack = []
+        val = 0
+        for i, token in enumerate(tokens):
+            if token not in "+-*/":
+                stack.append(int(token))
+            if token == "+" and stack:
+                val1 = stack.pop()
+                val2 = stack.pop()
+                stack.append(int(val1) + int(val2))
+            elif token == "-" and stack:
+                val1 = stack.pop()
+                val2 = stack.pop()
+                stack.append(int(val2) - int(val1))
+            elif token == "*" and stack:
+                val1 = stack.pop()
+                val2 = stack.pop()
+                stack.append(int(val2) * int(val1))
+            elif token == "/" and stack:
+                val1 = stack.pop()
+                val2 = stack.pop()
+                stack.append(int(int(val2) / int(val1)))
+
+        return stack[-1]
+
+
+        
