049c0df8d08ddf5dd4208826d518b9233ea07e10
Mihir Ganesan <mihirgan06@gmail.com>
2026-05-21 14:57:51 -0700

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        '''
+            I mean obv brute force is just that you have a hashmap and count each occurance return the integer that appears > 2 times
+
+
+            But obv this requires a linked list:
+                Convert
+
+            USE Slow and fast pointers
+        '''
+        slow, fast = 0,0
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
