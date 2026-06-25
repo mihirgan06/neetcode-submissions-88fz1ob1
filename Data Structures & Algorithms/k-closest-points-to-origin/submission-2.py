d4cce132ef685d405fb65afbbeb88432c8354de3
Mihir Ganesan <mihirgan06@gmail.com>
2026-06-25 15:18:48 -0400

    Add: k-closest-points-to-origin - submission-2

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-2.py
@@ -0,0 +1,28 @@
+import math
+import heapq
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        '''
+            Given a 2D array points, points[i] = [xi, yi]
+            also given an integer k, return the k closest points to origin
+
+            Compiute the distance with the origin
+            Create a heap with the heap with the distance return the k smallest from popping k times
+        
+        '''
+        heap = []
+        res = []
+        for x, y in points:
+            dist = (math.pow(x - x, 2) + math.pow(y - y, 2))
+            heap.append([dist, x, y])
+        heapq.heapify(heap)
+
+        for i in range(0, k):
+            dist, x, y = heapq.heappop(heap)
+            res.append([x,y])
+        return res
+        
+
+        
+        
+            
