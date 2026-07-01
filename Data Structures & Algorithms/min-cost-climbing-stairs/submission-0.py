ddc0ac85f695be6a6005eb59465c1d966f0c3433
Mihir Ganesan <mihirgan06@gmail.com>
2026-07-01 15:25:43 -0400

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        '''
+            Given an array of integers cost
+            cost[i] = cost of taking a step from the ith flore of a staircase
+            After payign the cost you can step to either i +1th floor or i + 2th floor
+
+            you may choose to start at index 0 or index 1
+            return min cost to reach the top ofthe staircase
+
+            cost = [1,2,1,2,1,1,1]
+
+            pay cost of 1, take two steps (total cost = 1)
+            pay cost of 1, take two steps (total cost = 2)
+            pay cost of 1, take two steps
+
+            pay cost of 1 
+            Total cost = 4
+
+        '''
+        def dfs(i):
+            if i >= len(cost):
+                return 0
+            return cost[i] + min(dfs(i + 1), dfs(i + 2)
+            )
+        return min(dfs(0), dfs(1))
+        
+        
+
+        
+
+        
