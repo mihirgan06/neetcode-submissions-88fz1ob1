48003f45442b36789f6a0ab7017a2c3372a4bffd
Mihir Ganesan <mihirgan06@gmail.com>
2026-07-02 16:56:57 -0400

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        '''
+            Bottom up Iterative idea:
+
+                Create dp array of size n
+
+                we either take the current or skip but dp[i]  is the max of the two ideas
+        '''
+        n = len(nums)
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        i = 2
+        while i < n:
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
+        return dp[-1]
+        
