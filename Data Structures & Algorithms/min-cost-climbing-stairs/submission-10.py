11b1059fc6e7be1c499698fd739d073aa295da10
Mihir Ganesan <mihirgan06@gmail.com>
2026-07-10 14:26:09 -0400

    Add: min-cost-climbing-stairs - submission-10

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-10.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        '''
+            Given array of integers cost, cost[i] cost from taking a step from the ith floor
+            After paying cost you can eitehr step to the i + 1 or i + 2 floor
+
+        '''
+        cache = [-1] * len(cost)
+
+        def dfs(i):
+            if i >= len(cost):
+                return 0
+            
+
+            
+            if cache[i] != -1:
+                return cache[i]
+            cache[i] = cost[i] + min(dfs(i + 1), dfs(i + 2))
+            
+            return cache[i]
+        return min(dfs(0), dfs(1))
+        
