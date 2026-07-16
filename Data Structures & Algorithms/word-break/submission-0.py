d21fc5b9c36c5e7847a3fec2f15a0cb31913a2e6
Mihir Ganesan <mihirgan06@gmail.com>
2026-07-16 15:14:24 -0400

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.py
@@ -0,0 +1,40 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        '''
+            Given a string s, and a dictionary woirdDict, 
+            return True if s can be segmnented into a space sperated sequence of dictionary words
+
+            s = "neetcode, wordDict = neet code
+            true
+
+            s = "applepenapple", wordDict = ["apple","pen","ape"]
+            true
+
+
+            
+            Input: s = "catsincars", wordDict = ["cats","cat","sin","in","car"]
+            car fails so false
+        
+            each phrase in worddict has to be a contiguous substring of s
+
+
+        '''
+        cache = [-1] * len(s)
+
+        def dfs(i):
+            if i == len(s):
+                return True
+            if cache[i] != -1:
+                return True
+            for word in wordDict:
+                if s[i:i + len(word)] == word:
+                    if dfs(i + len(word)):
+                        cache[i] = True
+                        return True
+            cache[i] = False
+            return False
+        return dfs(0)
+                
+                
+
+
