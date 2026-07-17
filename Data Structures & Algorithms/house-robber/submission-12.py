3d357f008a467cb466320c0565f88434c4228892
Mihir Ganesan <mihirgan06@gmail.com>
2026-07-17 19:48:38 -0400

    Add: house-robber - submission-12

---
+++ Data Structures & Algorithms/house-robber/submission-12.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        '''
+            Bottom up approach
+            dp array of size n
+
+        '''
+        n = len(nums)
+        dp = [0] * (n)
+        if n == 1:
+            return nums[0]
+        dp[0] = nums[0]
+        
+        dp[1] = max(nums[0], nums[1]) 
+        if len(nums) == 1:
+            return nums[0]
+        i = 2
+        
+        while i < n:
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
+            i += 1
+        return dp[-1]
+
+        
