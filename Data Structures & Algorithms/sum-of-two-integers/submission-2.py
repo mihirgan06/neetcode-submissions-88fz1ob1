5953607ece79c3c1f360ea5146cddc823fd9189f
Mihir Ganesan <mihirgan06@gmail.com>
2026-07-22 15:40:11 -0400

    Add: sum-of-two-integers - submission-2

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        '''
+            Get the sum of 2 integers without using +/- operators
+            a = 1, b = 1
+            a = 0001, b = 0001
+            0001
+            0001
+            = 0010, 2,
+            if we see two 1s in the same position we can always carry that over to the next place
+
+            a = 4, b = 7
+            a = 0100, b = 0111
+            res = 11 which is 1011
+            
+
+            So we should xor the two numbers at each place, and if we see 1 in both matching places we carry it over
+        '''
+        mask = 0xFFFFFFFF
+        max_int = 0x7FFFFFFF
+        while b != 0:
+            carry = ((a & b) << 1) & mask
+
+            a = (a ^ b) & mask
+            b = carry
+        if a <= max_int:
+            return a
+        
+        return ~(a ^ mask)
